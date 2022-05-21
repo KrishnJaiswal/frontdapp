a97341ff4174c1aacb575617648d174a4e8437c0
Krishna Jaiswal <jaiswalk633@gmail.com>
2022-05-21 14:26:00 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import {useState,useEffect} from "react";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
+import {ethers} from "ethers"
 import './App.css';
 
 function App() {
+  const [greeting,doGreeting]=useState(null);
+  const [contract,setContract]=useState(null);
+  const [provider,setProvider]=useState(null);
+  useEffect(() => {
+    const loadProvider=async ()=>{
+      let contractAddress="0x5fbdb2315678afecb367f032d93f642f64180aa3";
+      const url="http://localhost:8545";
+      const provider=new ethers.providers.JsonRpcProvider(url);
+      const contract=new ethers.Contract(
+        contractAddress,
+        Greeter.abi,
+        provider
+      )
+      setContract(contract)
+      setProvider(provider)
+      console.log(contract);        
+    };
+    loadProvider();
+  }, []);
+  useEffect(() => {
+    const getGreeting=async ()=>{
+      const greeting=await contract.greet();
+      doGreeting(greeting);
+      console.log(greeting);
+    }
+    contract&&getGreeting();
+  }, [contract]);
+  const changeGreetings=async ()=>{
+      const input=document.querySelector("#value");
+      const signer=contract.connect(provider.getSigner());
+      signer.setGreeting(input.value);
+      setTimeout(function(){
+        window.location.reload(1);
+      },500);
+      setTimeout();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h3>{greeting}</h3>
+      <input className="input" type="text" id="value"></input>
+      <button className="button" onClick={changeGreetings}>Change</button>
     </div>
   );
 }
